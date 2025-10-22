c37dd247020f29f2d622a46587d566803e5a070d
Akoto Etornam Tutu <etornamtutuakoto@gmail.com>
2025-10-22 08:03:45 +0000

    Implement header for global access

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+export function Header() {
+  return (
+    <div className="header-container">
+      <h1>
+        <span> &#128218; </span>Timetable Clash Checker
+      </h1>
+
+      <p>Manage courses and prevent scheduling conflicts</p>
+    </div>
+  );
+}
---
+++ src/components/auth/SignUp.jsx
@@ -0,0 +1,18 @@
+import { Header } from "../Header";
+export function SignUp() {
+  return (
+    <div>
+
+      <form action="" method="">
+        <label htmlFor="id-number">ID Number</label>
+        <input type="text" id="id-number" name="id-number" />
+
+        <label htmlFor="first-name">First Name</label>
+        <input type="text" id="id-number" name="id-number" />
+
+        <label htmlFor="id-number">ID Number</label>
+        <input type="text" id="id-number" name="id-number" />
+      </form>
+    </div>
+  );
+}
