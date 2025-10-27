8e5fbf5e38ebe511b8d51a2bf036ebfc592fe90b
selinaKudadze <kudadzeselina@gmail.com>
2025-10-27 19:56:35 +0000

    feat:provide routes for login and signup pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { SignUp } from "./components/auth/SignUp";
 import { Header } from "./components/Header";
-import Login from './components/auth/Login'
+import Login from './components/auth/Login';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
-      <SignUp />
-      <Login />
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
