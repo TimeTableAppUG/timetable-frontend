927dfb4d7a8f89e9174a0e27b829d33a9765e3dc
selinaKudadze <kudadzeselina@gmail.com>
2025-10-31 23:01:14 +0000

    feat:include ui design for clicking on a nav item

--- src/components/navBar/navItem.jsx
+++ src/components/navBar/navItem.jsx
@@ -5,10 +5,10 @@ export default function NavItem({ navName, navItemSelected }) {
 
 
     return (
-        <div className="grid justify-center items-center pt-5 font-semibold cursor-default">
+        <div className={navItemSelected == navName ? "flex flex-col justify-between items-center pt-5 font-semibold cursor-default w-40 bg-white/70" : "flex flex-col justify-between items-center pt-5 font-semibold cursor-default w-40"}>
             <p className={navItemSelected == navName ? 'text-indigo-600 text-center' : 'text-gray-600 text-center'}>{navName}</p>
             {navItemSelected == navName ?
-                <hr className="w-full pt-5 pl-5 pr-5 pb-0 border-2 border-indigo-600"></hr>
+                <hr className="w-full  border-2 rounded-sm mt-5 mr-5 ml-5 mb-0 border-indigo-600"></hr>
                 :
                 <div className="w-full pt-5 pl-5 pr-5 pb-0 "></div>
             }
