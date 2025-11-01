9b4ff57a3536b056805b8e57df43a7584270b642
selinakudadze <kudadzeselina@gmail.com>
2025-11-01 16:37:42 +0000

    feat:create utility for reading the excel sheet

---
+++ src/components/uploadTimetable/readExcelSheet.js
@@ -0,0 +1,23 @@
+import * as XLSX from "xlsx";
+
+export default readExcel = (file) => {
+  return new Promise((resolve, reject) => {
+    const excelReader = new FileReader();
+
+    excelReader.onload = (e) => {
+      const data = new Uint8Array(e.target.result);
+      const workbook = XLSX.read(data, { type: "array" });
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+      const excelData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+      console.log("EXCEL SHEET IN JSON FORMAT", excelData);
+      resolve(excelData);
+    };
+
+    excelReader.onerror = (error) => {
+      reject(error);
+    };
+
+    excelReader.readAsArrayBuffer(file);
+  });
+};
